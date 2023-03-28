c33df813e0242306a5998ccc09c242ca6b8d71fd
Calvin Remsburg <cremsburg.dev@gmail.com>
2023-03-28 07:47:14 -0500

    adding bootstrap, static files, and crispy forms

--- backend/django_project/settings.py
+++ backend/django_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # third party
+    "crispy_forms",
+    "crispy_bootstrap5",
     # local
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig",
@@ -125,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -137,3 +143,7 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 # redirect login and logout
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+# django crispy forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
--- backend/pages/tests.py
+++ backend/pages/tests.py
@@ -1,24 +1,53 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
 from django.urls import reverse, resolve
-from pages.views import HomePageView
+from .views import HomePageView, AboutPageView
 
 
-class HomepageTests(SimpleTestCase):
+class StaticPagesTests(TestCase):
     def setUp(self):
-        url = reverse("home")
-        self.response = self.client.get(url)
+        self.home_url = reverse("home")
+        self.about_url = reverse("about")
 
-    def test_url_exists_at_correct_location(self):
-        self.assertEqual(self.response.status_code, 200)
+    def test_home_page_status_code(self):
+        response = self.client.get(self.home_url)
+        self.assertEqual(response.status_code, 200)
 
-    def test_homepage_url_name(self):
-        self.assertEqual(resolve("/").func.view_class, HomePageView)
+    def test_about_page_status_code(self):
+        response = self.client.get(self.about_url)
+        self.assertEqual(response.status_code, 200)
 
-    def test_homepage_template(self):
-        self.assertTemplateUsed(self.response, "home.html")
+    def test_home_page_template(self):
+        response = self.client.get(self.home_url)
+        self.assertTemplateUsed(response, "home.html")
 
-    def test_homepage_contains_correct_html(self):
-        self.assertContains(self.response, "<h1>This is our home page.</h1>")
+    def test_about_page_template(self):
+        response = self.client.get(self.about_url)
+        self.assertTemplateUsed(response, "about.html")
 
-    def test_homepage_does_not_contain_incorrect_html(self):
-        self.assertNotContains(self.response, "This is some incorrect content.")
+    def test_home_page_contains_correct_html(self):
+        response = self.client.get(self.home_url)
+        self.assertContains(response, "<h1>This is our home page.</h1>")
+
+    def test_about_page_contains_correct_html(self):
+        response = self.client.get(self.about_url)
+        self.assertContains(response, "<h1>About page.</h1>")
+
+    def test_home_page_does_not_contain_incorrect_html(self):
+        response = self.client.get(self.home_url)
+        self.assertNotContains(
+            response, "Hi there! This text should not be on the page."
+        )
+
+    def test_about_page_does_not_contain_incorrect_html(self):
+        response = self.client.get(self.about_url)
+        self.assertNotContains(
+            response, "Hi there! This text should not be on the page."
+        )
+
+    def test_home_page_resolves(self):
+        resolver = resolve(self.home_url)
+        self.assertEqual(resolver.func.__name__, HomePageView.as_view().__name__)
+
+    def test_about_page_resolves(self):
+        resolver = resolve(self.about_url)
+        self.assertEqual(resolver.func.__name__, AboutPageView.as_view().__name__)
