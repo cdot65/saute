db5fc3d6799c2298d7ba526dad01201acbe7ece6
Calvin Remsburg <cremsburg.dev@gmail.com>
2023-04-03 09:20:51 -0500

    adding logic for login/logout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+    constructor(public cookieService: CookieService, private router: Router) {}
+
+    logout() {
+      this.cookieService.delete('auth_token');
+      this.router.navigate(['/login']);
+    }
+    title = 'frontend';
 }
