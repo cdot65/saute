db5058446537256a5d94a2c469664bb8fb6e1dea
Calvin Remsburg <cremsburg.dev@gmail.com>
2023-04-09 12:51:56 -0500

    moving create functions into panorama

--- frontend/src/app/inventory/panorama/panorama.component.ts
+++ frontend/src/app/inventory/panorama/panorama.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AuthService } from '../../auth.service';
 import { catchError, map } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { NgForm } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-panorama',
@@ -13,10 +15,21 @@ export class PanoramaComponent implements OnInit {
   panoramaData: any;
   displayedColumns: string[] = ['hostname', 'ipv4_address', 'ipv6_address', 'api_token', 'author', 'created_at'];
 
-  constructor(private http: HttpClient, private authService: AuthService) { }
+  constructor(private http: HttpClient, private cookieService: CookieService, private authService: AuthService) { }
+
+  // Add panorama-create related variables
+  showCreateForm = false;
+  panorama = {
+    hostname: '',
+    ipv4_address: '',
+    ipv6_address: '',
+    api_token: '',
+    author: ''
+  };
 
   ngOnInit(): void {
     this.fetchPanoramaData();
+    this.getCurrentUserId();
   }
 
   fetchPanoramaData() {
@@ -34,6 +47,47 @@ export class PanoramaComponent implements OnInit {
       .subscribe((data: any[]) => {
         this.panoramaData = data;
       });
+  }
+
+  // Add panorama-create related methods
+  getCurrentUserId() {
+    const authToken = this.cookieService.get('auth_token');
+    const headers = new HttpHeaders().set('Authorization', `Token ${authToken}`);
+
+    this.http.get<any[]>('http://localhost:8000/api/v1/users/', { headers })
+      .subscribe(response => {
+        const user = response[0];
+        this.panorama.author = user['id'];
+      });
+  }
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      const authToken = this.cookieService.get('auth_token');
+      const headers = new HttpHeaders().set('Authorization', `Token ${authToken}`);
+
+      this.http.post('http://localhost:8000/api/v1/panorama/', this.panorama, { headers })
+      .subscribe(
+        response => {
+          console.log('New panorama created:', response);
+          this.fetchPanoramaData();
+          this.resetForm();
+        },
+        error => {
+          console.error('Error creating panorama:', error);
+        }
+      );
+    }
+  }
+
+  resetForm() {
+    this.panorama = {
+      hostname: '',
+      ipv4_address: '',
+      ipv6_address: '',
+      api_token: '',
+      author: this.panorama.author
+    };
   }
 
   maskValue(value: string): string {
