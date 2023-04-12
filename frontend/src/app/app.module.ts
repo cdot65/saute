8dd5007543e397672f9eaf3484378de05a44ce31
Calvin Remsburg <cremsburg.dev@gmail.com>
2023-04-12 16:14:23 -0500

    add more form options

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,11 +12,14 @@ import { AuthInterceptor } from './auth.interceptor';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatOptionModule } from '@angular/material/core';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSortModule } from '@angular/material/sort';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -87,11 +90,14 @@ import { PanoramaRebootComponent } from './maintenance/panorama-reboot/panorama-
     MatButtonModule,
     MatCardModule,
     MatDialogModule,
+    MatFormFieldModule,
     MatIconModule,
     MatInputModule,
     MatListModule,
     MatMenuModule,
+    MatOptionModule,
     MatPaginatorModule,
+    MatSelectModule,
     MatSidenavModule,
     MatSortModule,
     MatSnackBarModule,
