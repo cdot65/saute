a4d4866c5d24ef95eefbeaaa3afee4ca932a1657
Calvin Remsburg <cremsburg.dev@gmail.com>
2023-04-19 11:30:13 -0500

    increase celery debugging

--- backend/django_project/celery.py
+++ backend/django_project/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.utils.log import get_task_logger
 from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
@@ -8,5 +9,11 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
 app = Celery("django_project")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
+logger = get_task_logger(__name__)
+
+app.log.setup_logging_subsystem()
+app.log.redirect_stdouts_to_logger(logger)
+app.log.setup_task_loggers(loglevel="DEBUG")  # Change this to 'INFO' if you prefer
 
 app.autodiscover_tasks()
