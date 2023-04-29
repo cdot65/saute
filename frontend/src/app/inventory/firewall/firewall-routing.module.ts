1cac29e54cd586d5cc243db8d2978908d363bd5f
Calvin Remsburg <cremsburg.dev@gmail.com>
2023-04-29 15:32:39 -0500

    firewall-list working

--- frontend/src/app/inventory/firewall/firewall-routing.module.ts
+++ frontend/src/app/inventory/firewall/firewall-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { FirewallCreateComponent } from "./firewall-create/firewall-create.component";
+import { FirewallListComponent } from "./firewall-list/firewall-list.component"; // Import the new component
 
 const routes: Routes = [
   {
     path: "create",
     component: FirewallCreateComponent,
+  },
+  {
+    path: "", // Add a new route for the firewall list component
+    component: FirewallListComponent,
   },
 ];
 
--- frontend/src/app/inventory/firewall/firewall.module.ts
+++ frontend/src/app/inventory/firewall/firewall.module.ts
@@ -15,9 +15,10 @@ import {
 
 import { FirewallRoutingModule } from "./firewall-routing.module";
 import { FirewallCreateComponent } from "./firewall-create/firewall-create.component";
+import { FirewallListComponent } from "./firewall-list/firewall-list.component"; // Import the new component
 
 @NgModule({
-  declarations: [FirewallCreateComponent],
+  declarations: [FirewallCreateComponent, FirewallListComponent], // Include the new component
   imports: [
     CommonModule,
     FormsModule,
