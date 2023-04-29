8450d0562df12f22aaa5ea190bd0b65e32cde9c4
Calvin Remsburg <cremsburg.dev@gmail.com>
2023-04-29 17:28:47 -0500

    panorama details

--- frontend/src/app/inventory/panorama/panorama-routing.module.ts
+++ frontend/src/app/inventory/panorama/panorama-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { PanoramaCreateComponent } from "./panorama-create/panorama-create.component";
+import { PanoramaListComponent } from "./panorama-list/panorama-list.component";
+import { PanoramaDetailsComponent } from "./panorama-details/panorama-details.component"; // Import the new component
 
 const routes: Routes = [
+  {
+    path: "",
+    component: PanoramaListComponent,
+  },
   {
     path: "create",
     component: PanoramaCreateComponent,
   },
+  {
+    path: "details/:id",
+    component: PanoramaDetailsComponent, // Add the route for the new component
+  },
+  // Add any additional routes here as needed
 ];
 
 @NgModule({
@@ -14,3 +25,4 @@ const routes: Routes = [
   exports: [RouterModule],
 })
 export class PanoramaRoutingModule {}
+
--- frontend/src/app/inventory/panorama/panorama.module.ts
+++ frontend/src/app/inventory/panorama/panorama.module.ts
@@ -14,9 +14,15 @@ import {
 
 import { PanoramaRoutingModule } from "./panorama-routing.module";
 import { PanoramaCreateComponent } from "./panorama-create/panorama-create.component";
+import { PanoramaListComponent } from "./panorama-list/panorama-list.component";
+import { PanoramaDetailsComponent } from "./panorama-details/panorama-details.component"; // Import the new component
 
 @NgModule({
-  declarations: [PanoramaCreateComponent],
+  declarations: [
+    PanoramaCreateComponent,
+    PanoramaListComponent,
+    PanoramaDetailsComponent, // Add the new component to declarations
+  ],
   imports: [
     CommonModule,
     FormsModule,
