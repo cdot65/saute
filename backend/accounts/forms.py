2a8989240cebd689e1717f5bf7388550004a48d7
Calvin Remsburg <cremsburg.dev@gmail.com>
2023-05-03 09:59:07 -0500

    circle cropping of profile images

--- backend/accounts/forms.py
+++ backend/accounts/forms.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-
 from .models import CustomUser
 
 
@@ -7,6 +6,13 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
         fields = UserCreationForm.Meta.fields + ("name", "profile_image")
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.profile_image.name = f"{instance.profile_image.name.split('.')[0]}.png"
+        if commit:
+            instance.save()
+        return instance
 
 
 class CustomUserChangeForm(UserChangeForm):
