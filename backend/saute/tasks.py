0afdc3e923f381b749bb008477d86ba494f3ee0a
Calvin Remsburg <cremsburg.dev@gmail.com>
2023-05-19 11:00:05 -0500

    adding sendgrid api key from environs

--- backend/saute/tasks.py
+++ backend/saute/tasks.py
@@ -7,6 +7,9 @@ from celery import shared_task
 from django.contrib.auth import get_user_model
 from saute.models import Jobs
 
+# third party library imports
+from environs import Env
+
 # import our python scripts
 from saute.scripts import (
     run_admin_report,
@@ -20,16 +23,31 @@ from saute.scripts import (
 # Configure logging
 # ----------------------------------------------------------------------------
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s"
+    level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(message)s"
+)
+
+# ----------------------------------------------------------------------------
+# Load environment variables from .env file
+# ----------------------------------------------------------------------------
+env = Env()
+env.read_env()
+
+sendgrid_api_key = env(
+    "SENDGRID_API_KEY",
+    "go to https://docs.sendgrid.com/ui/account-and-settings/api-keys",
 )
 
 sys.path.append("/code/backend")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
 django.setup()
 User = get_user_model()
 
 
-@shared_task(bind=True)  # Add bind=True to access task instance
+# ----------------------------------------------------------------------------
+# Export Security Rules to CSV file
+# ----------------------------------------------------------------------------
+@shared_task(bind=True)
 def execute_export_rules_to_csv(self, pan_url, api_token, author_id):
     # Retrieve the user object by id
     author = User.objects.get(id=author_id)
@@ -39,7 +57,7 @@ def execute_export_rules_to_csv(self, pan_url, api_token, author_id):
         job_type="export_rules_to_csv",
         json_data=None,
         author=author,
-        task_id=self.request.id,  # Set the task_id field in the Jobs entry
+        task_id=self.request.id,
     )
 
     try:
@@ -52,7 +70,10 @@ def execute_export_rules_to_csv(self, pan_url, api_token, author_id):
     job.save()
 
 
-@shared_task(bind=True)  # Add bind=True to access task instance
+# ----------------------------------------------------------------------------
+# Get Panorama System Info
+# ----------------------------------------------------------------------------
+@shared_task(bind=True)
 def execute_get_system_info(self, pan_url, api_token, author_id):
     # Retrieve the user object by id
     author = User.objects.get(id=author_id)
@@ -75,7 +96,10 @@ def execute_get_system_info(self, pan_url, api_token, author_id):
     job.save()
 
 
-@shared_task(bind=True)  # Add bind=True to access task instance
+# ----------------------------------------------------------------------------
+# Retrieve and upload Certificate Chain
+# ----------------------------------------------------------------------------
+@shared_task(bind=True)
 def execute_upload_cert_chain(self, api_token, author_id, pan_url, url):
     # Retrieve the user object by id
     author = User.objects.get(id=author_id)
@@ -98,7 +122,10 @@ def execute_upload_cert_chain(self, api_token, author_id, pan_url, url):
     job.save()
 
 
-@shared_task(bind=True)  # Add bind=True to access task instance
+# ----------------------------------------------------------------------------
+# Sync Panorama to Prisma
+# ----------------------------------------------------------------------------
+@shared_task(bind=True)
 def execute_sync_to_prisma(
     self,
     pan_url,
@@ -132,7 +159,10 @@ def execute_sync_to_prisma(
     job.save()
 
 
-@shared_task(bind=True)  # Add bind=True to access task instance
+# ----------------------------------------------------------------------------
+# Report of Administrators
+# ----------------------------------------------------------------------------
+@shared_task(bind=True)
 def execute_admin_report(
     self,
     pan_url,
@@ -153,11 +183,12 @@ def execute_admin_report(
     logging.info(f"Job ID: {job.pk}")
 
     try:
-        json_report = run_admin_report(pan_url, api_token, to_emails)
+        json_report = run_admin_report(pan_url, api_token, to_emails, sendgrid_api_key)
         logging.info(json_report)
         job.json_data = json_report
         logging.info(job)
     except Exception as e:
+        logging.error(e)
         job.result = f"Job ID: {job.pk}\nError: {e}"
 
     # Save the updated job information
