25e721351ea216423e47f568c5f8d7362d34a2ed
Calvin Remsburg <cremsburg.dev@gmail.com>
2023-05-22 16:36:10 -0400

    add all option

--- frontend/src/app/automation/operations/assurance-snapshots/assurance-snapshots.component.ts
+++ frontend/src/app/automation/operations/assurance-snapshots/assurance-snapshots.component.ts
@@ -15,12 +15,29 @@ export class AssuranceSnapshotsComponent implements OnInit {
 
   // Added checkboxes object
   checkboxes = {
-    one: false,
-    two: false,
-    three: false,
-    four: false,
-    five: false,
+    all: false,
+    arp_table: false,
+    content_version: false,
+    ip_sec_tunnels: false,
+    license: false,
+    nics: false,
+    routes: false,
+    session_stats: false,
   };
+
+  onAllChange(): void {
+    let newState = this.checkboxes.all;
+    this.checkboxes = {
+      all: newState,
+      arp_table: newState,
+      content_version: newState,
+      ip_sec_tunnels: newState,
+      license: newState,
+      nics: newState,
+      routes: newState,
+      session_stats: newState,
+    };
+  }
 
   constructor(
     private firewallService: FirewallService,
