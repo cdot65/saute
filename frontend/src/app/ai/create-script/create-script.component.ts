978b8a70d06d25f0c485584333421c2ce3728254
Calvin Remsburg <cremsburg.dev@gmail.com>
2023-05-30 16:42:54 -0500

    remove fluff

--- frontend/src/app/ai/create-script/create-script.component.ts
+++ frontend/src/app/ai/create-script/create-script.component.ts
@@ -150,17 +150,4 @@ export class CreateScriptComponent implements OnInit, OnDestroy {
       console.log("Form is not valid");
     }
   }
-
-  getAccordionBodyText(value: string) {
-    const textSample = `
-      <strong>This is the <mark>#${value}</mark> item accordion body.</strong> It is hidden by
-      default, until the collapse plugin adds the appropriate classes that we use to
-      style each element. These classes control the overall appearance, as well as
-      the showing and hiding via CSS transitions. You can modify any of this with
-      custom CSS or overriding our default variables. It&#39;s also worth noting
-      that just about any HTML can go within the <code>.accordion-body</code>,
-      though the transition does limit overflow.
-    `;
-    return this.sanitizer.bypassSecurityTrustHtml(textSample);
-  }
 }
