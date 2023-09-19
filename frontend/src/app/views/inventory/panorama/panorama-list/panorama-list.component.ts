10dc68da774fe0b6dc51c0ab6993f9e238ba34e1
Calvin Remsburg <cremsburg.dev@gmail.com>
2023-09-19 07:24:13 -0500

    panorama list

--- frontend/src/app/views/inventory/panorama/panorama-list/panorama-list.component.ts
+++ frontend/src/app/views/inventory/panorama/panorama-list/panorama-list.component.ts
@@ -1,90 +1,89 @@
+/**
+ * PanoramaListComponent handles fetching, displaying, and managing the
+ * list of ingredients in the inventory.
+ */
 import { Component, OnInit } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
 
-import { CookieService } from "ngx-cookie-service";
+import { Panorama } from "../../../../shared/interfaces/panorama.interface";
+import { PanoramaService } from "../../../../shared/services/panorama.service";
 import { Router } from "@angular/router";
-import Swal from "sweetalert2";
-import { catchError } from "rxjs/operators";
-import { environment } from "src/environments/environment";
-import { of } from "rxjs";
 
 @Component({
   selector: "app-panorama-list",
   templateUrl: "./panorama-list.component.html",
 })
-export class PanoramaListComponent implements OnInit {
-  private API_URL = environment.apiUrl;
-  panoramaData: any[];
 
+/**
+ * PanoramaListComponent is an Angular component for displaying and managing
+ * the list of ingredients in the inventory.
+ */
+export class PanoramaListComponent implements OnInit {
+  // Array to store the fetched panorama data
+  panoramaData: Panorama[];
+  detailsVisible: { [key: string]: boolean } = {};
+
+  /**
+   * Constructor for the PanoramaListComponent; injects required
+   * services and initializes the ingredientData array.
+   */
   constructor(
-    private http: HttpClient,
-    private router: Router,
-    private cookieService: CookieService
+    private panoramaService: PanoramaService,
+    private router: Router
   ) {
     this.panoramaData = [];
   }
 
+  // Angular lifecycle hook that initializes the component
   ngOnInit() {
-    this.panoramaInventory();
+    this.fetchPanoramaData();
   }
 
-  // Fetch data from the API
-  panoramaInventory() {
-    this.http
-      .get<any[]>(`${this.API_URL}/api/v1/panorama/inventory/`)
-      .pipe(
-        catchError((error) => {
-          console.error("Error fetching Panorama data:", error);
-          return of([]);
-        })
-      )
-      .subscribe((data: any[]) => {
-        this.panoramaData = data;
-      });
-  }
+  /**
+   * Fetches the panorama data from the PanoramaService and
+   * stores it in the ingredientData array.
+   */
 
-  showDeleteConfirmationDialog(entryId: number): void {
-    Swal.fire({
-      title: "Are you sure?",
-      text: "You won't be able to revert this!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonText: "Yes, delete it!",
-      cancelButtonText: "No, cancel!",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.deleteEntry(entryId);
-        Swal.fire("Deleted!", "The entry has been deleted.", "success");
-      }
+  fetchPanoramaData() {
+    this.panoramaService.panoramaInventory().subscribe((data: Panorama[]) => {
+      this.panoramaData = data;
     });
   }
 
-  deleteEntry(entryId: number): void {
-    const authToken = this.cookieService.get("auth_token");
-    const headers = new HttpHeaders().set(
-      "Authorization",
-      `Token ${authToken}`
-    );
-    this.http
-      .delete(`${this.API_URL}/api/v1/panorama/inventory/${entryId}/`, {
-        headers,
-      })
-      .subscribe(
-        (response) => {
-          // console.log("Panorama instance deleted:", response);
-          this.panoramaInventory();
-        },
-        (error) => {
-          console.error("Error deleting Panorama instance:", error);
-        }
-      );
+  /**
+   * Deletes an existing panorama entry based on the provided UUID.
+   *
+   * @param uuid - The UUID of the panorama entry to delete
+   */
+  deleteEntry(uuid: string): void {
+    this.panoramaService.deleteEntry(uuid).subscribe(() => {
+      this.fetchPanoramaData();
+    });
   }
 
+  /**
+   * Navigates to the "/inventory/panorama/create" route when the user clicks
+   * the "Create New Recipe" button.
+   */
   navigateToCreate(): void {
     this.router.navigate(["/inventory/panorama/create"]);
   }
 
-  navigateToDetails(id: number): void {
-    this.router.navigate(["/inventory/panorama/details", id]);
+  /**
+   * Navigates to the "/inventory/panorama/details" route with the provided UUID
+   * when the user clicks the "Edit" button.
+   *
+   * @param uuid - UUID of the panorama entry to edit
+   */
+  navigateToDetails(uuid: string): void {
+    this.router.navigate(["/inventory/panorama/details", uuid]);
+  }
+
+  /**
+   * Toggle the row to reveal edit and delete buttons.
+   *
+   * @param uuid - UUID of the panorama entry to edit
+   */
+  toggleDetails(uuid: string): void {
+    this.detailsVisible[uuid] = !this.detailsVisible[uuid];
   }
 }
